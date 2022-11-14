7881cb7e421323196314b82887b59f93681a3ed2
Elyor <elyorbilolovelyorbilolov@gmail.com>
2022-11-14 14:14:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,154 @@
+//5#. O'zgaruvchilar
+
+/* let number = 10;
+const borderColor = 'red';
+
+const person = {
+    name: 'Elyor',
+};
+
+person.name = 'Bilol';
+console.log(person);
+
+// Old version
+
+console.log(clientName);
+var clientName = 'Samar'; */
+
+
+
+
+//6#. Qat'iy rejim
+
+/* "use strict";
+
+const number = 10;
+const clientName = 'Samar';
+
+console.log(number); */
+
+
+
+
+//7#. Ma'lumot turlari
+/*
+1) primitive      2) Object
+
+Boolean       /   Array
+null         /    Object
+undefined   /     function
+number     /      RegEx
+String    /       Date
+Symbol
+ */
+/*
+"use strict";
+
+//Number
+let number = 5.6;  // Data Type = Number
+console.log(-8 / 0); // Infinity
+console.log('Samar' * 8); // Nan
+
+// String
+const clientName = 'Samar';
+
+//Boolean
+const isMarried = false;
+const isCircleEarth = true;
+// true = xa | to'g'ri
+// false = yoq | notog'ri
+
+//null (yo'q o'zgaruvchini chaqirsek null o'zgaruvchisi chiqadi)
+console.log(clientAge);
+
+// undefined (o'zgaruvchi bor lekin malumoti yoq bolsa undefined chiqadi)
+let und;
+console.log(und);
+
+// Object
+
+const thief = {
+    //key: value
+    age: 30, //number
+    jacket: 'black', //string
+    isLong: false //boolean
+};
+console.log(thief.age);
+
+//array
+const colors = ['red', 'black', 'green', 10, [], {}];
+console.log(colors[1]);
+ */
+
+
+
+
+//#8. Foydalanuvchi bilan oddiy muloqot
+/*
+'use strict';
+
+alert('Hello Java');
+
+const place = confirm('Are you here?');
+console.log(place);
+
+const currentPlace = prompt('Whera are you?', 'Samara');
+console.log(currentPlace);
+
+const age = +prompt('How old are you?', '18');
+console.log(age);
+ */
+
+
+
+//#9. Interpolatsiya
+/*
+'use strict';
+
+const channel = prompt('What is your favourite youtuber');
+console.log(`https://youtube.com/${channel}`);
+
+const user = 'Elyor';
+const job = 'JavaScript developer';
+
+console.log(`User name is ${user}, he is ${job}`);
+ */
+
+
+
+//#10. Operatorlar
+/*
+'use strict';
+
+console.log(6 + 'g');
+
+let incr = 10;
+let decr = 10;
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(10 % 3);
+
+console.log(5 * 5 == '25'); //qiymatni
+console.log(5 * 5 === '25'); // qiymat va malumot turini
+
+// && = va || = yoki
+
+const isAge = true;
+const isClose = true;
+const isChecked = false;
+
+console.log(isAge && isClose && !isChecked);
+
+console.log(2 + 2 * 2 !== 6);
+ */
+
+
+
+
+//#11. Github bilan ishlash
+
+
+
+
