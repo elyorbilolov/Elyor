17c05361c5824766325e5fa6ab149144080c1a28
Elyor <elyorbilolovelyorbilolov@gmail.com>
2022-11-18 15:31:40 +0300

    new

--- script.js
+++ script.js
@@ -400,7 +400,7 @@ writeGenres();
 
 
 //#19. Callback
-/* 
+/*
 function first(cb) {
     setTimeout(() => {
         console.log(1);
@@ -425,5 +425,48 @@ function done() {
 
 edu('JavaScript', done);
  */
+
+
+
+//#20. Object. Destruptizatsiya
+
+/*
+const theif = {
+    jacket: 'black',
+    height: 1.7,
+    colors: {
+        hair: 'gray',
+        style: 'curley',
+    },
+    howOut: function () {
+        console.log('Fast with help doors');
+    },
+};
+theif.howOut();
+
+const { hair, style } = theif.colors;
+
+console.log(hair);
+console.log(style);
+ */
+
+/*
+console.log(Object.keys(theif).length); // massivga olib beradi
+
+for (let key in theif) {
+   if (typeof theif[key] === 'object') {
+        for (let i in theif[key]) {
+            console.log(`Property ${i} has value ${theif[key][i]}`);
+        }
+    } else {
+        console.log(`Property ${key} has value ${theif[key]}`);
+    }
+
+} */
+
+
+//#21. Massivlar
+
+
 
 
