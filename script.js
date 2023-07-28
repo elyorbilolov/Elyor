163261f11d11ccbeff46772990385e032564058c
Elyor <elyorbilolovelyorbilolov@gmail.com>
2023-07-28 18:48:21 +0500

    28.07.2023

--- script.js
+++ script.js
@@ -1898,50 +1898,218 @@ window.addEventListener("DOMContentLoaded", () => {
 
     //slider
 
-    const slides = document.querySelectorAll(".offer__slide"),
+    /*     const slides = document.querySelectorAll(".offer__slide"),
         next = document.querySelector(".offer__slider-next"),
         prev = document.querySelector(".offer__slider-prev"),
         total = document.querySelector("#total"),
-        current = document.querySelector("#current");
+        current = document.querySelector("#current"),
+        slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+        slidesField = document.querySelector(".offer__slider-inner"),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slider = document.querySelector(".offer__slider");
 
     let slideIndex = 1;
+    let offset = 0;
 
-    showSlides(slideIndex);
+    //---***---
+    //     CAROUSEL SLIDER
+    //---***---
 
     if (slides.length < 10) {
         total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
     } else {
         total.textContent = slides.length;
+        current.textContent = slideIndex;
     }
 
-    function showSlides(index) {
-        if (index > slides.length) {
+    slidesField.style.width = 100 * slides.length + "%";
+    slidesField.style.display = "flex";
+    slidesField.style.transition = ".5s ease all";
+    slidesWrapper.style.overflow = "hidden";
+
+    slides.forEach((slide) => {
+        slide.style.width = width;
+    });
+
+    const indicators = document.createElement("ol");
+    const dots = [];
+    indicators.classList.add("carousel-indicators");
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement("li");
+        dot.setAttribute("data-slide-to", i + 1);
+        dot.classList.add("carousel-dot");
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    next.addEventListener("click", () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
             slideIndex = 1;
+        } else {
+            slideIndex++;
         }
-        if (index < 1) {
-            slideIndex = slides.length;
-        }
-        slides.forEach((item) => (item.style.display = "none"));
-        slides[slideIndex - 1].style.display = "block";
 
         if (slides.length < 10) {
             current.textContent = `0${slideIndex}`;
         } else {
             current.textContent = slideIndex;
         }
-    }
 
-    function plusSlides(index) {
-        showSlides((slideIndex += index));
-    }
-
-    next.addEventListener("click", () => {
-        plusSlides(1);
+        dots.forEach((dot) => (dot.style.opacity = ".5"));
+        dots[slideIndex - 1].style.opacity = 1;
     });
 
     prev.addEventListener("click", () => {
-        plusSlides(-1);
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+
+        dots.forEach((dot) => (dot.style.opacity = ".5"));
+        dots[slideIndex - 1].style.opacity = 1;
     });
+
+    dots.forEach((dot) => {
+        dot.addEventListener("click", (e) => {
+            const slideTo = e.target.getAttribute("data-slide-to");
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach((dot) => (dot.style.opacity = ".5"));
+            dots[slideIndex - 1].style.opacity = 1;
+        });
+    });
+ */
+
+    const slides = document.querySelectorAll(".offer__slide"),
+        next = document.querySelector(".offer__slider-next"),
+        prev = document.querySelector(".offer__slider-prev"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current"),
+        slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+        slidesField = document.querySelector(".offer__slider-inner"),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slider = document.querySelector(".offer__slider");
+
+    let slideIndex = 1;
+    let offset = 0;
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    slides.forEach((slide) => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener("click", () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    prev.addEventListener("click", () => {
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    // showSlides(slideIndex);
+
+    // if (slides.length < 10) {
+    //     total.textContent = `0${slides.length}`;
+    // } else {
+    //     total.textContent = slides.length;
+    // }
+
+    // function showSlides(index) {
+    //     if (index > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     if (index < 1) {
+    //         slideIndex = slides.length;
+    //     }
+    //     slides.forEach((item) => (item.style.display = "none"));
+    //     slides[slideIndex - 1].style.display = "block";
+
+    //     if (slides.length < 10) {
+    //         current.textContent = `0${slideIndex}`;
+    //     } else {
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+
+    // function plusSlides(index) {
+    //     showSlides((slideIndex += index));
+    // }
+
+    // next.addEventListener("click", () => {
+    //     plusSlides(1);
+    // });
+
+    // prev.addEventListener("click", () => {
+    //     plusSlides(-1);
+    // });
 });
 
 // #54. JSON Chuqur clonlash
