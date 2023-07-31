2acf023dc36ddf63f5c88944dab03eb4ffe786f0
Elyor <elyorbilolovelyorbilolov@gmail.com>
2023-07-31 18:52:53 +0500

    31.07.2023

--- script.js
+++ script.js
@@ -2016,64 +2016,6 @@ window.addEventListener("DOMContentLoaded", () => {
     });
  */
 
-    const slides = document.querySelectorAll(".offer__slide"),
-        next = document.querySelector(".offer__slider-next"),
-        prev = document.querySelector(".offer__slider-prev"),
-        total = document.querySelector("#total"),
-        current = document.querySelector("#current"),
-        slidesWrapper = document.querySelector(".offer__slider-wrapper"),
-        slidesField = document.querySelector(".offer__slider-inner"),
-        width = window.getComputedStyle(slidesWrapper).width,
-        slider = document.querySelector(".offer__slider");
-
-    let slideIndex = 1;
-    let offset = 0;
-
-    if (slides.length < 10) {
-        total.textContent = `0${slides.length}`;
-        current.textContent = `0${slideIndex}`;
-    } else {
-        total.textContent = slides.length;
-        current.textContent = slideIndex;
-    }
-
-    slides.forEach((slide) => {
-        slide.style.width = width;
-    });
-
-    next.addEventListener("click", () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
-            offset = 0;
-        } else {
-            offset += +width.slice(0, width.length - 2);
-        }
-        if (slideIndex == slides.length) {
-            slideIndex = 1;
-        } else {
-            slideIndex++;
-        }
-
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
-    });
-
-    prev.addEventListener("click", () => {
-        if (slideIndex == 1) {
-            slideIndex = slides.length;
-        } else {
-            slideIndex--;
-        }
-
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
-    });
-
     // showSlides(slideIndex);
 
     // if (slides.length < 10) {
@@ -2110,6 +2052,8 @@ window.addEventListener("DOMContentLoaded", () => {
     // prev.addEventListener("click", () => {
     //     plusSlides(-1);
     // });
+
+    const 
 });
 
 // #54. JSON Chuqur clonlash
